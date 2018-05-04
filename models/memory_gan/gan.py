d989b7d01b64c9a4f521ab6f3ceea608585be9fb
rogergirgis <roger.girgis@gmail.com>
2018-05-04 17:10:01 -0400

    - Made a new method to implement the no_EM version. Not yet implemented. - Made another method for my testing of new update_memory method.

--- models/memory_gan/gan.py
+++ models/memory_gan/gan.py
@@ -9,7 +9,7 @@ from models.gan_super import gan_super
 class GAN(gan_super):
     """GAN
 	"""
-    def __init__(self, dataset, is_cuda):
+    def __init__(self, dataset, is_cuda, use_EM):
         """
         """
         super().__init__(dataset, is_cuda)
@@ -18,6 +18,7 @@ class GAN(gan_super):
         self.mcgn = mcgn(f_dim=self.f_dim, fc_dim=self.fc_dim, z_dim=self.z_dim,
                          c_dim=self.c_dim, key_dim=self.key_dim)
         self.memory = memory(key_dim=self.key_dim, memory_size=self.mem_size, choose_k=self.choose_k, is_cuda=is_cuda)
+        self.use_EM = use_EM
 
     def discriminate(self, x, label):
         q = self.dmn.forward(x)  # get query vec
@@ -25,11 +26,14 @@ class GAN(gan_super):
         qn2 = q.div(torch.transpose(qn.expand(q.size(1), q.size(0)), 1, 0))
         self.q = qn2
         post_prob = self.memory.query(qn2)
-        #self.memory.update_memory(qn2, label)
+        if self.use_EM:
+            self.memory.Roger_update_memory(qn2, label)
+        else:
+            self.memory.update_memory_noEM(qn2, label)
         return post_prob
 
     def generate(self, z):
-        if self.cuda:
+        if self.is_cuda:
             key = self.memory.sample_key(z.shape[0]).cuda()
         else:
             key = self.memory.sample_key(z.shape[0])
--- models/memory_gan/memory.py
+++ models/memory_gan/memory.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
+
 class memory(nn.Module):
     """DISCRIMINATIVE MEMORY NETWORK """
     def __init__(self, key_dim, memory_size, choose_k, is_cuda):
@@ -109,6 +110,58 @@ class memory(nn.Module):
                 k_hat = (k_hat/torch.norm(k_hat, p=2, dim=0))  # l2 normalize
                 self.memory_key[idx_to_change] = k_hat
                 self.memory_hist[idx_to_change] = h_hat
+
+    def update_memory_noEM(self, q, label):
+        pass
+
+    def Roger_update_memory(self, q, label, n_steps=1):
+        # Goal: compute P(x|c, v_c=y)
+        # Start with finding indices where mem_value=label
+        indices = ((self.memory_values == label[0]) == 1).nonzero().squeeze()  # this assumes all labels are the same.
+        reduced_memory = self.memory_key[indices]  # memory with only the correct classes
+
+        # compute similarity between batch of q and my mem_keys with correct class
+        similarities = torch.matmul(q, torch.t(reduced_memory))
+        p_x_given_c_unnorm = torch.exp(similarities)
+
+        #  compute P(c)
+        p_c_unnorm = self.memory_hist[indices] + self.beta
+
+        #  compute P(c|x, v_c=y) from eq 1 of paper
+        p_c_given_x_v = (p_x_given_c_unnorm * p_c_unnorm)
+        _, idxs = torch.topk(p_c_given_x_v, k=self.choose_k)
+
+        for i, l in enumerate(idxs):
+            if list(indices.size())[0] == 0:  # I don't think this ever happens...
+                #  find oldest memory slot and copy information onto it
+                oldest_idx = torch.argmax(self.memory_age)
+                self.memory_key[oldest_idx] = q[i]
+                self.memory_hist[oldest_idx] = self.memory_hist.mean()
+                self.memory_age[oldest_idx] = 0
+                self.memory_values[oldest_idx] = label[i]
+            else:
+                gamma = 0
+                alpha = 0.5
+                h_hat = alpha * self.memory_hist[idxs[i]]
+                k_hat = reduced_memory[idxs[i]]
+                for _ in range(n_steps):
+                    #  E Step
+                    similarities = torch.matmul(q[i], torch.t(k_hat))
+                    p_x_given_c_unnorm = torch.exp(similarities).detach()
+                    p_c = ((h_hat + self.beta) / torch.sum(h_hat + self.beta))
+                    p_c_given_x = (p_x_given_c_unnorm * p_c)/(torch.matmul(p_x_given_c_unnorm, p_c))
+                    next_gamma = p_c_given_x
+
+                    #  M step
+                    h_hat += next_gamma - gamma
+                    k_hat += torch.transpose(((next_gamma - gamma) / h_hat).
+                                             expand(self.key_dim, len(h_hat)), 0, 1)*(q[i] - k_hat)
+                    gamma = next_gamma
+                    k_hat = (k_hat/torch.norm(k_hat, p=2, dim=0))  # l2 normalize
+
+                self.memory_key[idxs[i]] = k_hat
+                self.memory_hist[idxs[i]] = h_hat
+        self.memory_age[:] += 1  # I guess...
 
     def sample_key(self, batch_size):
         real_hist = self.memory_hist * self.memory_values
